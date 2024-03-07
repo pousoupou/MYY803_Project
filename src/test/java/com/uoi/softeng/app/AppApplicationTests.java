b32de7b53e7ec85cdc309a5303eb799795727a79
pousoupou <stelios.papaspiros@gmail.com>
2024-03-07 23:37:43 +0200

    init

---
+++ src/test/java/com/uoi/softeng/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uoi.softeng.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
