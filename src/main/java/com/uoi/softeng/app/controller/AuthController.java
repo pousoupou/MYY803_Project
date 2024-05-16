59a0efea64a76ed7005ff56272160530d3c9d7d7
IoannisBouzas <cs05025@uoi.gr>
2024-05-16 20:39:00 +0300

    register works fine

--- src/main/java/com/uoi/softeng/app/controller/AuthController.java
+++ src/main/java/com/uoi/softeng/app/controller/AuthController.java
@@ -6,9 +6,9 @@ import com.uoi.softeng.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,8 +25,8 @@ public class AuthController {
 
     @RequestMapping("/register")
     public String register(Model model) {
-        UserProfile userProfile = new UserProfile();
-        model.addAttribute("userProfile", userProfile);
+        User user = new User();
+        model.addAttribute("user", user);
 
         return "register";
     }
